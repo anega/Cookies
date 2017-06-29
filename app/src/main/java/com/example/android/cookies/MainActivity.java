e767ad1e363fe406482037beb29e639400e1e678
anega <anegamega@gmail.com>
2017-06-29 16:52:15 +0300

    Add drawables; change img and text onClick

--- app/src/main/java/com/example/android/cookies/MainActivity.java
+++ app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.cookies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        // TODO: Find a reference to the ImageView in the layout. Change the image.
+        ImageView mainImg = (ImageView) findViewById(R.id.android_cookie_image_view);
+        mainImg.setImageResource(R.drawable.after_cookie);
+        // TODO: Find a reference to the TextView in the layout. Change the text.
+        TextView statusText = (TextView) findViewById(R.id.status_text_view);
+        statusText.setText("I'm so full");
     }
 }
